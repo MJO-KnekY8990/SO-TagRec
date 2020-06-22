45581d116c5a9b14f765ef0f16ec2857ffa33e7e
MJO-KnekY <mathieu.josset@gmail.com>
2020-06-22 19:41:35 +0200

    make it better

--- API_prj5.py
+++ API_prj5.py
@@ -2,8 +2,6 @@ import numpy as np
 import pandas as pd
 import re
 import streamlit as st
-import seaborn as sns
-import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 from sklearn.feature_extraction.text import TfidfVectorizer
 import gensim
